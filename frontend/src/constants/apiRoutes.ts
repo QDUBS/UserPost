ba95baab63c7f281185fd999afe8b9de3a54cb9c
qdubsawsprojects@gmail.com <qdubsmusk@gmail.com>
2025-11-29 10:28:19 +0100

    chore: updated base url

--- frontend/src/constants/apiRoutes.ts
+++ frontend/src/constants/apiRoutes.ts
@@ -1,6 +1,8 @@
 export const APIRoutes = {
   VITE_API_URL:
-    process.env.VITE_API_URL || "https://userpost-y1xm.onrender.com",
+    process.env.VITE_API_URL ||
+    "https://userpost-y1xm.onrender.com" ||
+    "http://localhost:3001",
   USERS: "/users",
   POSTS: "/posts",
 };
